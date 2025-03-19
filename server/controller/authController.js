c4fccbad4b02ca5ca5ddc30c0285e30cedc85b8d
Shreekant <shreekant4062@gmail.com>
2025-03-19 19:48:22 +0530

    refactor signup process and update welcome email content for improved user experience

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -25,9 +25,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-  });
+    password: req.body.password,  });
   const url = `${req.protocol}://${req.get("host")}/me`;
   await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, req, res);
